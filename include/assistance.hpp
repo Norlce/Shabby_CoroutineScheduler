c2191334b75944afedd6ea885b8d3cdc9b9f18de
Norlce <norlce@foxmail.com>
2024-08-01 23:50:48 +0800

    fix:无法正确获取当前协程id

--- include/assistance.hpp
+++ include/assistance.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 using co_id_t = std::size_t;
+constexpr co_id_t NOP_ID = 0;
+
 using priority_t = std::size_t;
 constexpr priority_t HIGHEST_LEVEL = 0xffffffff'ffffffff;
 constexpr priority_t LOWEST_LEVEL = 0x00;
@@ -21,15 +23,16 @@ T get_next_iterator(T& it){
 }
 
 struct awaiter_assister{
-    uint64_t bb;
+    co_id_t id;
     bool await_ready()noexcept { return false;}
 
     void await_suspend(std::coroutine_handle<> h)noexcept {
+        id = reinterpret_cast<co_id_t>(h.address());
         h.resume();
     }
 
     uint64_t await_resume()noexcept {
-        return bb;
+        return id;
     }
 };
 
