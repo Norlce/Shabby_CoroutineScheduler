3f3fb00d4d34bb5e474e6a697c51f9244cf0ffce
Norlce <norlce@foxmail.com>
2024-08-13 23:47:03 +0800

    refactor:更新示例文件example.cpp

--- example.cpp
+++ example.cpp
@@ -1,10 +1,12 @@
 #include"./include/Scheduler.hpp"
+#include "assistance.hpp"
+#include "coroutine_base.hpp"
 #include <chrono>
 #include <string>
 #include <thread>
 #include<iostream>
 
-coroutine_states<std::string, int,std::string> func(std::string coro_num){
+coro<std::string, int,std::string> func(std::string coro_num){
     auto p = current_corotine_id();
     std::cout<<coro_num<<':'<<"coro_handle: "<<p<<std::endl;
     int i = 0;
@@ -16,7 +18,7 @@ coroutine_states<std::string, int,std::string> func(std::string coro_num){
     std::cout<<coro_num<<" done"<<std::endl;
 }
 
-coroutine_states<std::string, int,std::string> func2(std::string num){
+coro<std::string, int,std::string> func2(std::string num){
     auto p = current_corotine_id();
     std::cout<<num<<':'<<"coro_handle:"<<std::hex<<' '<<p<<std::endl;
     for(int i = 0; i<10; i++){
@@ -25,7 +27,7 @@ coroutine_states<std::string, int,std::string> func2(std::string num){
     }
 }
 
-coroutine_states<void> func3(std::string num){
+coro<void> func3(std::string num){
     auto p = current_corotine_id();
     std::cout<<num<<':'<<"coro_handle: "<<p<<std::endl;
     int i = 0;
@@ -40,27 +42,25 @@ int main(){
     using type = decltype(func("co"));
     auto num = 1;
     std::cout<<"############################################# Scheduler_"<<num<<" #############################################"<<std::endl;
-    {
         Scheduler<type> sche(
             func("co"), 
-            coroutine_packer(func("co2"), 3),
-            coroutine_packer(func("co3"), 1)
+            copacker(func("co2"), 3),
+            copacker(func("co3"), 1)
             );
+    {
+        sche.automatic();
+        sche.set_max_threads(4);
         auto running_id = sche.get_running_id_vector();
         std::cout<<"running_id:";
         for(auto id:running_id){
             std::cout<<id<<' ';
         }
         std::cout<<std::endl;
-        auto f = [&sche](){
-            sche.continuous();
-        };
-        std::thread t(f);
-        std::thread y(f);
-        std::thread tt(f);
-        std::thread yy(f);
-        sche.push_coroutine(func("co4"));
-        sche.push_coroutine(coroutine_packer(func("co5"), HIGHEST_LEVEL));
+        sche.add_co(func("co4"));
+        sche.add_co(copacker(func("co5"), HIGHEST_LEVEL));
+        auto finish_id = sche.get_finishied_id_vector();
+        
+        
         for(auto &id:running_id){
             std::this_thread::sleep_for(std::chrono::milliseconds(50));
             std::cout<<id<<std::endl;
@@ -68,34 +68,29 @@ int main(){
             // for(auto [co, i, num]:values){
             //     std::cout<<co<<' '<<i<<' '<<num<<std::endl;
             // }
-            std::cout<<"values.size():"<<values.size()<<std::endl;
+            std::cout<<"finish.size():"<<values.size()<<std::endl;
         }
-        t.join();
-        y.join();
-        tt.join();
-        yy.join();
         
     }
     
     std::cout<<"############################################# Scheduler_"<<num++<<" #############################################\n"<<std::endl;
 
     std::cout<<"############################################# Scheduler_"<<num<<" #############################################"<<std::endl;
-    Scheduler<> sche2(func3("sche"), func3("sche2"), func3("sche3"), func3("sche4"));
+    sche.intterput();
+    Scheduler<> sche2(
+        func3("sche"),
+        func3("sche2"), 
+        copacker(func3("sche3"), HIGHEST_LEVEL), 
+        func3("sche4"));
     {
-        auto f = [&sche2](){
-            sche2.continuous();
-        };
-        std::thread t(f);
-        std::thread y(f);
-        std::thread tt(f);
-        std::thread yy(f);
-        t.join();
-        y.join();
-        tt.join();
-        yy.join();
+        sche2.automatic();
+        sche2.set_max_threads(4);
+        sche.intterput(false);
+        sche2.start();
+        sche2.intterput();
     }
     std::cout<<"############################################# Scheduler_"<<num++<<" #############################################\n"<<std::endl;
-
+    sche2.intterput(false);
     std::cout<<"-----------------------------------------------end of all-----------------------------------------------"<<std::endl;
     return 0;
 }
